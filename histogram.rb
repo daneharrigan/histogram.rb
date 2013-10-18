2eee4e554f74f8b1518be83a33cae8e4b85d2e7a
Dane Harrigan <dane.harrigan@gmail.com>
2013-10-18 16:47:57 -0700

    update algorithm

--- histogram.rb
+++ histogram.rb
@@ -11,13 +11,40 @@ class Histogram
   end
 
   def update(bin)
-    if false
+    if b = @bins.detect { |b| b.point == bin.point }
+      b.count += 1
     else
       @bins << bin
       @bins.sort! { |a, b| a.point <=> b.point }
 
-      gap_idx = -1
-      gap_val
+      if @bins.size <= @max
+        return
+      end
+
+      while @bins.size > @max do
+        gap_idx = -1
+        gap_val = Float::MAX
+
+        @bins[0..-2].each_with_index do |cur, i|
+          nxt = @bins[i+1]
+          gap = nxt.point - cur.point
+
+          if gap < gap_val
+            gap_val = gap
+            gap_idx = i
+          end
+        end
+
+        cur = @bins[gap_idx]
+        nxt = @bins[gap_idx+1]
+
+        m = (cur.count + nxt.count)
+        p = (cur.point + nxt.point) / m
+
+        @bins -= [cur, nxt]
+        @bins << Bin.new(p, m)
+        @bins.sort! { |a, b| a.point <=> b.point }
+      end
     end
   end
 
@@ -35,8 +62,12 @@ class Histogram
     attr :count
 
     def initialize(point, count)
-      @point = point
-      @count = count
+      @point = point.to_f
+      @count = count.to_f
+    end
+
+    def count=(v)
+      @count = v
     end
   end
 end
--- histogram_test.rb
+++ histogram_test.rb
@@ -4,14 +4,13 @@ require "debugger"
 require "histogram"
 
 class HistogramTest < Minitest::Test
-  def setup
-  end
-
   def test_10_points_in_5_bins
     histogram = Histogram.new(5)
-    [23, 19, 10, 16, 36, 2, 9, 32, 30, 45].each do |n|
+    [23, 19, 10, 16, 36, 2, 9].each do |n| #, 32, 30, 45].each do |n|
       histogram.insert(n)
     end
+
+    assert_equal 5, histogram.bins.size
 
     assert_equal 2, histogram.bins[0].point
     assert_equal 1, histogram.bins[0].count
@@ -27,5 +26,18 @@ class HistogramTest < Minitest::Test
 
     assert_equal 36, histogram.bins[4].point
     assert_equal 1, histogram.bins[4].count
+  end
+
+  def test_1_to_10_twice
+    histogram = Histogram.new(10)
+    10.times { |n| histogram.insert(n) }
+    10.times { |n| histogram.insert(n) }
+
+    assert_equal 10, histogram.bins.size
+
+    10.times do |n|
+      assert_equal n, histogram.bins[n].point
+      assert_equal 2, histogram.bins[n].count
+    end
   end
 end
